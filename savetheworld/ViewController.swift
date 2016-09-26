11ee3e8b7c6ae4984dc4bc6f280dd25a364886e2
Waleed Al-otaibi <waleedal-otaibi@MacBook-Pro-alkhas-b-Waleed.local>
2016-09-26 19:46:08 +0300

    Initial Commit

---
+++ savetheworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savetheworld
+//
+//  Created by Waleed Al-otaibi on 12/25/1437 AH.
+//  Copyright © 1437 Waleed Al-otaibi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
